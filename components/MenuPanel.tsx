e5f6abc358b6b3bb7f857d78e14e314f8f6b20f7
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-09-11 16:38:24 +0300

    style fix

--- components/MenuPanel.tsx
+++ components/MenuPanel.tsx
@@ -5,9 +5,6 @@ import React from "react";
 import { useRouter } from "next/router";
 
 export default function Menu() {
-  function handleMainPageClick() {
-    return "";
-  }
 
   const router = useRouter();
 
