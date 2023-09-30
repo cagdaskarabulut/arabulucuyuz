d2bfc037ce9e2d72ca9458501b4c1791998dc09b
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-10-01 01:42:16 +0300

    admin panel infrastructure built

--- components/MessageList.js
+++ components/MessageList.js
@@ -2,6 +2,7 @@
 import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 const MessageList = () => {
   const [messages, setMessages] = useState([]);
@@ -19,9 +20,40 @@ const MessageList = () => {
 
   return (
     <div>
-      test test test
-      <br />
-      {messages.map(message => <div key={message.id}>{message.content}</div>)}
+
+<TableContainer >
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>İsim</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Telefon</TableCell>
+            <TableCell align="right">Yaratılma Tarihi</TableCell>
+            <TableCell align="right">Not</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {messages.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                {row.name}
+                </TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.phoneNumber}</TableCell>
+                <TableCell align="right">{row.creationDate}</TableCell>
+                <TableCell align="right">{row.content}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    {/* {messages.map(message => <div key={message.id}>{message.content}</div>)} */}
+
+
     </div>
   )
 }
--- pages/AdminPanel.js
+++ pages/AdminPanel.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import MessageList from "../components/MessageList";
 
 const AdminPanel = () => {
   return (
     <div>
-      Admin Panel
+      <h1>Hoşgeldin Öznur, </h1>
+      <br />
+      <h3>Mesaj Listesi</h3>
+      <MessageList />
     </div>
   );
 };
