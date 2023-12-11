2283e3b3a8f258a6ff361731d555cf2796d098f2
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-12-11 08:50:05 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <>
       <MetaPanel
-        title="Arabuluculuk nedir? - Avukat Arabulucu Öznur İlhan Karabulut - Arabuluculuk ve Hukuk Danışmanlığı"
+        title="Arabuluculuk nedir? - Avukat Arabulucu Öznur İlhan Karabulut - Arabuluculuk ve Hukuk Danışmanlığı - İstanbul Arabulucu"
         descriptionContent="Avukat Arabulucu Öznur İlhan Karabulut, pek çok dava konusunda deneyime sahip bir avukat ve arabulucudur. Arabuluculuk ve hukuk danışmanlığı alanında uzmanlık sunar. İhtiyaçlarınıza özel çözümler için bize başvurun ve hukuki sorunlarınızı çözümleyin."
-        keywordsContent="Uzman Arabulucu, İş Hukuku Arabulucusu, Kira Hukuku Arabulucusu, İş Hukuku, Kira Hukuku, Ortaklığın Giderilmesi, Mal Paylaşımı, Ortaklığın Giderilmesi Arabuluculuğu, Avukat Arabulucu, Öznur İlhan Karabulut, Arabuluculuk, Hukuk Danışmanlığı, Hukuki Uzmanlık, Hukuki Destek, Uyuşmazlık Çözümü, Deneyimli Hukukçu"
+        keywordsContent="istanbul arabulucu, Uzman Arabulucu, İş Hukuku Arabulucusu, Kira Hukuku Arabulucusu, İş Hukuku, Kira Hukuku, Ortaklığın Giderilmesi, Mal Paylaşımı, Ortaklığın Giderilmesi Arabuluculuğu, Avukat Arabulucu, Öznur İlhan Karabulut, Arabuluculuk, Hukuk Danışmanlığı, Hukuki Uzmanlık, Hukuki Destek, Uyuşmazlık Çözümü, Deneyimli Hukukçu, ıstanbul arabulucu"
         imagePath="/images/Arabulucu.jpg"
         imageAlt="Arabulucu"
       />
